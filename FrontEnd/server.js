b79ade7da32bbccc7ce50e51d34952e7e5a91172
Oluwalana Ajayi <125604535+lanacreates@users.noreply.github.com>
2023-06-30 22:11:00 +0100

    Update server.js

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -5,3 +5,51 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Welcome to the Carbon Credit Trading Platform!'));
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
+
+const Web3 = require('web3');
+const ContractKit = require('@celo/contractkit');
+
+let web3 = new Web3('https://alfajores-forno.celo-testnet.org');
+let kit = ContractKit.newKitFromWeb3(web3);
+
+const contractAddress = 'Your_Contract_Address_Here';  // Replace with your deployed contract address
+
+let carbonCredit = new web3.eth.Contract(abi, contractAddress);
+
+// Issue Carbon Credits
+app.get('/issue', async (req, res) => {
+    let issue = await carbonCredit.methods.issue('account_address', amount).send({ from: 'account_address' });
+    res.send(issue);
+});
+
+// Trade Carbon Credits
+app.get('/trade', async (req, res) => {
+    let trade = await carbonCredit.methods.transfer('receiver_address', amount).send({ from: 'sender_address' });
+    res.send(trade);
+});
+
+// Retire Carbon Credits
+app.get('/retire', async (req, res) => {
+    let retire = await carbonCredit.methods.retire('account_address', amount).send({ from: 'account_address' });
+    res.send(retire);
+});
+
+const marketplaceAddress = 'Your_Marketplace_Contract_Address_Here';  // Replace with your deployed marketplace contract address
+
+let marketplace = new web3.eth.Contract(abi, marketplaceAddress);
+
+// Buy Carbon Credits
+app.get('/buy', async (req, res) => {
+    let buy = await marketplace.methods.buyCarbonCredits('buyer_address', amount).send({ from: 'buyer_address', value: web3.utils.toWei(amount.toString(), "ether") });
+    res.send(buy);
+});
+
+// Sell Carbon Credits
+app.get('/sell', async (req, res) => {
+    let sell = await marketplace.methods.sellCarbonCredits('seller_address', amount).send({ from: 'seller_address' });
+    res.send(sell);
+});
+
+/// Please make sure to replace 'Your_Contract_Address_Here', 'Your_Marketplace_Contract_Address_Here',
+/// 'account_address', 'receiver_address', 'sender_address', 'buyer_address', and 'seller_address' with the appropriate Ethereum addresses and contract addresses for your specific setup.
+/// Additionally, make sure to import the necessary dependencies and define the abi variable, which represents the ABI (Application Binary Interface) of your contracts.
